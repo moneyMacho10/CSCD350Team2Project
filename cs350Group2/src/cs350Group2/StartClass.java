740ebbd97c0866a9ea53a032e28c93c1130c48ac
moneyMacho10 <57733220+moneyMacho10@users.noreply.github.com>
2020-11-23 21:14:43 -0800

    Add files via upload

---
+++ cs350Group2/src/cs350Group2/StartClass.java
@@ -0,0 +1,37 @@
+
+package cs350Group2;
+import cs350f20project.Startup;
+import cs350f20project.controller.ActionProcessor;
+import cs350f20project.controller.Controller;
+import cs350f20project.controller.cli.CommandLineInterface;
+import cs350f20project.controller.cli.parser.CommandParser;
+import cs350f20project.controller.cli.parser.MyParserHelper;
+//import cs350f20project.controller.cli.parser.CommandParser;
+//import cs350f20project.controller.cli.parser.MyParserHelper;
+//import cs350f20project.controller.cli.parser.ParseException;
+import cs350f20project.controller.cli.parser.ParseException;
+
+
+public class StartClass {
+
+	public static void main(String[] args) throws ParseException  {
+		
+		Startup startup = new Startup();
+		
+		startup.go();
+		
+		
+		
+		//MyParserHelper parserHelper = new MyParserHelper();
+		MyParserHelper parserHelper = new MyParserHelper(new ActionProcessor(new CommandLineInterface(new Controller())));
+		
+		String commandText = "@exit";
+		CommandParser parser = new CommandParser(parserHelper, commandText);
+		parser.parse();
+			
+	
+		
+
+	}
+
+}
